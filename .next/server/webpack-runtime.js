8c30798be64d0a1b7089f73c8188257e0e5374fe
myzhaffar <myzhaffar@gmail.com>
2025-07-13 20:39:32 +0700

    delete unused stuff

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("f4c79cf66f65bd5b")
+/******/ 		__webpack_require__.h = () => ("3aa3d2dc16317896")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
