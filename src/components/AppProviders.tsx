9d97e5dfa05a305c246e38af7bebb447c955ba5a
myzhaffar <myzhaffar@gmail.com>
2025-08-21 21:29:28 +0700

    cleanup: remove unused components, hooks, and dependencies

--- src/components/AppProviders.tsx
+++ src/components/AppProviders.tsx
@@ -3,7 +3,7 @@ import { AuthProvider } from '@/contexts/AuthContext';
 import { LanguageProvider } from '@/contexts/LanguageContext';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { TooltipProvider } from '@/components/ui/tooltip';
-import { Toaster as HotToaster } from 'react-hot-toast';
+
 import { Toaster } from '@/components/ui/toaster';
 import { Toaster as Sonner } from '@/components/ui/sonner';
 
@@ -18,7 +18,7 @@ export default function AppProviders({ children }: { children: React.ReactNode }
             {children}
             <Toaster />
             <Sonner />
-            <HotToaster />
+
           </AuthProvider>
         </LanguageProvider>
       </TooltipProvider>
