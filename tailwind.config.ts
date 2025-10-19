62b3b500eb37db5fb81d790f81ed6afec9da0129
myzhaffar <myzhaffar@gmail.com>
2025-10-19 20:09:26 +0700

    remove dark mode

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,6 @@ import tailwindcssAnimate from "tailwindcss-animate";
 import typography from "@tailwindcss/typography";
 
 const config: Config = {
-	darkMode: ["class"],
 	content: [
 		"./pages/**/*.{ts,tsx}",
 		"./components/**/*.{ts,tsx}",
